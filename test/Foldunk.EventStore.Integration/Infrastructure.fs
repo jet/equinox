2173fc7352403eae27211500dfa5429dd42cbfd4
Ruben Bartelink <ruben@jet.com>
2017-09-12 22:17:31 +0100

    Move ES tests to their own suite

--- test/Foldunk.EventStore.Integration/Infrastructure.fs
+++ test/Foldunk.EventStore.Integration/Infrastructure.fs
@@ -9,9 +9,8 @@ open Serilog
 open System
 
 type FsCheckGenerators =
-    //static member CartId = Arb.generate |> Gen.map CartId |> Arb.fromGen
     static member SkuId = Arb.generate |> Gen.map SkuId |> Arb.fromGen
-    //static member RequestId = Arb.generate |> Gen.map RequestId |> Arb.fromGen
+    static member RequestId = Arb.generate |> Gen.map RequestId |> Arb.fromGen
 
 type AutoDataAttribute() =
     inherit FsCheck.Xunit.PropertyAttribute(Arbitrary = [|typeof<FsCheckGenerators>|], MaxTest = 1, QuietOnSuccess = true)
@@ -20,3 +19,16 @@ let createLogger hookObservers =
     LoggerConfiguration()
         .WriteTo.Observers(System.Action<_> hookObservers)
         .CreateLogger()
+
+type LogCaptureBuffer() =
+    let captured = ResizeArray()
+    member __.Subscribe(source: IObservable<Serilog.Events.LogEvent>) =
+        source.Subscribe captured.Add
+    member __.Clear () = captured.Clear()
+    member __.Entries = captured.ToArray()
+    member __.ExternalCalls =
+        [ for i in captured do
+            let hasProp name = i.Properties.ContainsKey name 
+            let prop name = (string i.Properties.[name]).Trim '"' 
+            if hasProp "ExternalCall" && prop "ExternalCall" = "True" then
+                yield prop "Action" ]
