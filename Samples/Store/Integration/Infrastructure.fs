8128b870c88ead6c76dbe40eb405bd1d896c5f6b
Ruben Bartelink <ruben@jet.com>
2017-09-26 00:28:37 +0100

    Spike SerilogTracerAdapter

--- Samples/Store/Integration/Infrastructure.fs
+++ Samples/Store/Integration/Infrastructure.fs
@@ -4,6 +4,7 @@ module Example.Integration.Infrastructure
 open Domain
 open FsCheck
 open Serilog
+open Serilog.Events
 open System
 
 type FsCheckGenerators =
@@ -47,6 +48,48 @@ type LogCaptureBuffer() =
             let prop name = (string i.Properties.[name]).Trim '"'
             if hasProp "ExternalCall" && prop "ExternalCall" = "True" then
                 yield prop "Action" ]
+
+type SerilogTracerAdapter(emit) =
+    let (|SerilogScalar|_|) : LogEventPropertyValue -> obj option = function
+        | (:? ScalarValue as x) -> Some x.Value
+        | _ -> None
+    let formatter = Serilog.Formatting.Display.MessageTemplateTextFormatter("{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}", null);
+    let writeSerilogEvent (logEvent : Serilog.Events.LogEvent) =
+        try 
+            let tgp name =
+                match logEvent.Properties.TryGetValue name with
+                | true, value -> Some value
+                | false, _ -> None
+            let level =
+                match logEvent.Level with
+                | Serilog.Events.LogEventLevel.Fatal -> "Fatal"
+                | Serilog.Events.LogEventLevel.Error -> "Error"
+                | Serilog.Events.LogEventLevel.Information -> "Info"
+                | Serilog.Events.LogEventLevel.Verbose -> "Trace"
+                | Serilog.Events.LogEventLevel.Debug -> "Debug"
+                | Serilog.Events.LogEventLevel.Warning | _ -> "Warn"
+            match tgp "ExternalCall", tgp "Action", tgp "Latency" with
+            | Some (SerilogScalar (:? bool as externalCall)), Some (SerilogScalar (:? string as action)), Some (SerilogScalar (:? TimeSpan as latency))
+                    when externalCall ->
+                let others =
+                    seq {
+                        for x in logEvent.Properties do
+                            match x.Key with
+                            | "ExternalCall" | "Action" | "Latency" -> ()
+                            | _ -> yield x
+                    }
+                let othersRendered = [ for x in others -> sprintf "%s=%O" x.Key x.Value] |> String.concat ", "
+                sprintf "%s %s-Elapsed=%O Values: %s" level action latency othersRendered |> emit
+            | _ ->
+                use writer = new System.IO.StringWriter()
+                formatter.Format(logEvent, writer)
+                writer |> string |> emit
+          with _ -> 
+                use writer = new System.IO.StringWriter()
+                formatter.Format(logEvent, writer);
+                writer |> string |> sprintf "Cannnot log event: %s" |> emit
+    member __.Subscribe(source: IObservable<Serilog.Events.LogEvent>) =
+        source.Subscribe writeSerilogEvent
 
 /// Needs an ES instance with default settings
 /// TL;DR: At an elevated command prompt: choco install eventstore-oss; \ProgramData\chocolatey\bin\EventStore.ClusterNode.exe
---
+++ Samples/Store/Integration/LogIntegration.fs
@@ -0,0 +1,34 @@
+﻿module Example.Integration.LogIntegration
+
+open Domain
+open Backend
+open Swensen.Unquote
+open System
+open System.Diagnostics
+
+#nowarn "1182" // From hereon in, we may have some 'unused' privates (the tests)
+
+let createServiceWithEventStore eventStoreConnection = Favorites.Service(createGesStreamer eventStoreConnection 500, 3)
+
+type Tests() =
+    let createLoggerWithCapture () =
+        let capture = SerilogTracerAdapter(System.Diagnostics.Trace.WriteLine)
+        let subscribeLogListeners obs =
+            obs |> capture.Subscribe |> ignore
+        createLogger subscribeLogListeners, capture
+
+    [<AutoData>]
+    let ``Can trap and emit log entries`` clientId command = Async.RunSynchronously <| async {
+        let! conn = connectToLocalEventStoreNode ()
+        let log, _ = createLoggerWithCapture ()
+        let service = createServiceWithEventStore conn
+
+        do! service.Run log clientId command
+        let! items = service.Load log clientId
+
+        match command with
+        | Favorites.Commands.Favorite (_,skuIds) ->
+            test <@ skuIds |> List.forall (fun skuId -> items |> Array.exists (function { skuId = itemSkuId} -> itemSkuId = skuId)) @>
+        | _ ->
+            test <@ Array.isEmpty items@>
+    }
