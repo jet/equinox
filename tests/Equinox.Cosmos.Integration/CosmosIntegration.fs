2539e28934ce5c51daf15a6e8d067594f5861ae5
Dongdong.cai <dongdong.cai69@gmail.com>
2018-06-27 17:39:37 -0400

    changes regarding reviews

--- tests/Equinox.Cosmos.Integration/CosmosIntegration.fs
+++ tests/Equinox.Cosmos.Integration/CosmosIntegration.fs
@@ -8,7 +8,8 @@ open System
 
 /// Standing up an Equinox instance is complicated; to run for test purposes either:
 /// - replace connection below with a connection string or Uri+Key for an initialized Equinox instance
-/// - Create a local Equinox with dbName "test" and collectionName "test" using  provisioning script
+/// - Create a local Equinox with dbName "test" and collectionName "test" using script:
+///   /src/Equinox.Cosmos/EquinoxManager.fsx
 let connectToLocalEquinoxNode (log: Serilog.ILogger) =
     EqxConnector(log=log, requestTimeout=TimeSpan.FromSeconds 3., maxRetryAttemptsOnThrottledRequests=2, maxRetryWaitTimeInSeconds=60)
        .Establish("localDocDbSim", Discovery.UriAndKey(Uri "https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="))
