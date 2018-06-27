07b664845023a8cc1fb59e740860897d7a0fea1c
dongdongcai <huyuhuyu123@gmail.com>
2018-06-27 11:00:48 -0400

    Some naming changes

---
+++ tests/Equinox.Cosmos.Integration/Infrastructure.fs
@@ -0,0 +1,74 @@
+﻿[<AutoOpen>]
+module Equinox.Cosmos.Integration.Infrastructure
+
+open Domain
+open FsCheck
+open System
+
+type FsCheckGenerators =
+    static member SkuId = Arb.generate |> Gen.map SkuId |> Arb.fromGen
+    static member RequestId = Arb.generate |> Gen.map RequestId |> Arb.fromGen
+    static member ContactPreferencesId =
+        Arb.generate<Guid>
+        |> Gen.map (fun x -> sprintf "%s@test.com" (x.ToString("N")))
+        |> Gen.map ContactPreferences.Id
+        |> Arb.fromGen
+
+type AutoDataAttribute() =
+    inherit FsCheck.Xunit.PropertyAttribute(Arbitrary = [|typeof<FsCheckGenerators>|], MaxTest = 1, QuietOnSuccess = true)
+
+// Derived from https://github.com/damianh/CapturingLogOutputWithXunit2AndParallelTests
+// NB VS does not surface these atm, but other test runners / test reports do
+type TestOutputAdapter(testOutput : Xunit.Abstractions.ITestOutputHelper) =
+    let formatter = Serilog.Formatting.Display.MessageTemplateTextFormatter("{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}", null);
+    let writeSerilogEvent logEvent =
+        use writer = new System.IO.StringWriter()
+        formatter.Format(logEvent, writer);
+        writer |> string |> testOutput.WriteLine
+    interface Serilog.Core.ILogEventSink with member __.Emit logEvent = writeSerilogEvent logEvent
+
+[<AutoOpen>]
+module SerilogHelpers =
+    open Serilog
+    open Serilog.Events
+
+    let createLogger sink =
+        LoggerConfiguration()
+            .WriteTo.Sink(sink)
+            .CreateLogger()
+
+    let (|SerilogScalar|_|) : Serilog.Events.LogEventPropertyValue -> obj option = function
+        | (:? ScalarValue as x) -> Some x.Value
+        | _ -> None
+    [<RequireQualifiedAccess>]
+    type EqxAct = Append | AppendConflict | SliceForward | SliceBackward | BatchForward | BatchBackward
+    let (|EqxAction|) (evt : Equinox.Cosmos.Log.Event) =
+        match evt with
+        | Equinox.Cosmos.Log.WriteSuccess _ -> EqxAct.Append
+        | Equinox.Cosmos.Log.WriteConflict _ -> EqxAct.AppendConflict
+        | Equinox.Cosmos.Log.Slice (Equinox.Cosmos.Direction.Forward,_) -> EqxAct.SliceForward
+        | Equinox.Cosmos.Log.Slice (Equinox.Cosmos.Direction.Backward,_) -> EqxAct.SliceBackward
+        | Equinox.Cosmos.Log.Batch (Equinox.Cosmos.Direction.Forward,_,_) -> EqxAct.BatchForward
+        | Equinox.Cosmos.Log.Batch (Equinox.Cosmos.Direction.Backward,_,_) -> EqxAct.BatchBackward
+    let (|EqxEvent|_|) (logEvent : LogEvent) : Equinox.Cosmos.Log.Event option =
+        logEvent.Properties.Values |> Seq.tryPick (function
+            | SerilogScalar (:? Equinox.Cosmos.Log.Event as e) -> Some e
+            | _ -> None)
+
+    let (|HasProp|_|) (name : string) (e : LogEvent) : LogEventPropertyValue option =
+        match e.Properties.TryGetValue name with
+        | true, (SerilogScalar _ as s) -> Some s | _ -> None
+        | _ -> None
+    let (|SerilogString|_|) : LogEventPropertyValue -> string option = function SerilogScalar (:? string as y) -> Some y | _ -> None
+    let (|SerilogBool|_|) : LogEventPropertyValue -> bool option = function SerilogScalar (:? bool as y) -> Some y | _ -> None
+
+    type LogCaptureBuffer() =
+        let captured = ResizeArray()
+        let writeSerilogEvent (logEvent: LogEvent) =
+            logEvent.RenderMessage () |> System.Diagnostics.Trace.WriteLine
+            captured.Add logEvent
+        interface Serilog.Core.ILogEventSink with member __.Emit logEvent = writeSerilogEvent logEvent
+        member __.Clear () = captured.Clear()
+        member __.Entries = captured.ToArray()
+        member __.ChooseCalls chooser = captured |> Seq.choose chooser |> List.ofSeq
+        member __.ExternalCalls = __.ChooseCalls (function EqxEvent (EqxAction act) -> Some act | _ -> None)
