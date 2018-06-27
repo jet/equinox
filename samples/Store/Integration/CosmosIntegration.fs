8764f95803bf6e9f377d0672cba9f7cf5343fc62
dongdongcai <huyuhuyu123@gmail.com>
2018-06-27 11:00:48 -0400

    Some naming changes

--- samples/Store/Integration/CosmosIntegration.fs
+++ samples/Store/Integration/CosmosIntegration.fs
@@ -4,13 +4,16 @@ module Samples.Store.Integration.CosmosIntegration
 open Equinox.Cosmos
 open System
 
-/// Create an Equinox is compalicated,
-/// To run the test,
-/// Either replace connection below with a real equinox,
-/// Or create a local Equinox using script: https://jet-tfs.visualstudio.com/Jet/Marvel/_git/marvel?path=%2Fservices%2Feqx-man%2Fexample.fsx&version=GBmaster&_a=contents
-/// create Equinox with dbName "test" and collectionName "test" to perform test
-let connectToLocalEquinoxNode() =
-    EqxConnector(requestTimeout=TimeSpan.FromSeconds 3., maxRetryAttemptsOnThrottledRequests=2, maxRetryWaitTimeInSeconds=60)
-        .Connect(Discovery.UriAndKey((Uri "https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "test", "test")))
+/// Standing up an Equinox instance is complicated; to run for test purposes either:
+/// - replace connection below with a connection string or Uri+Key for an initialized Equinox instance
+/// - Create a local Equinox with dbName "test" and collectionName "test" using script:
+///   /src/Equinox.Cosmos/EquinoxManager.fsx
+let connectToLocalEquinoxNode log =
+    EqxConnector(log=log, requestTimeout=TimeSpan.FromSeconds 3., maxRetryAttemptsOnThrottledRequests=2, maxRetryWaitTimeInSeconds=60)
+        .Establish("equinoxStoreSampleIntegration", Discovery.UriAndKey(Uri "https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="))
 let defaultBatchSize = 500
-let createEqxGateway connection batchSize = EqxGateway(EqxConnection(connection), EqxBatchingPolicy(maxBatchSize = batchSize))
+let createEqxGateway connection batchSize = EqxGateway(connection, EqxBatchingPolicy(maxBatchSize = batchSize))
+// Typically, one will split different categories of stream into Cosmos collections - hard coding this is thus an oversimplification
+let (|StreamArgs|) streamName =
+    let databaseId, collectionId = "test", "test"
+    databaseId, collectionId, streamName
