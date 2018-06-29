76a21080394b1fb78ee76b0efbbed1163b789689
Ruben Bartelink <ruben@jet.com>
2018-06-29 01:39:07 +0100

    Avoid allocating intermediate string when encoding objects

--- src/Foldunk.Serialization/UnionCodec.fs
+++ src/Foldunk.Serialization/UnionCodec.fs
@@ -1,17 +1,22 @@
 module Foldunk.UnionCodec
 
 open Newtonsoft.Json
+open System.IO
 open TypeShape
 
 /// Newtonsoft.Json implementation of IEncoder that encodes direct to a UTF-8 Buffer
 type JsonUtf8Encoder(settings : JsonSerializerSettings) =
+    let serializer = JsonSerializer.Create(settings)
     interface UnionEncoder.IEncoder<byte[]> with
         member __.Encode (value : 'T) =
-            JsonConvert.SerializeObject(value, settings)
-            |> System.Text.Encoding.UTF8.GetBytes
-        member __.Decode (json : byte[]) =
-            let x = System.Text.Encoding.UTF8.GetString(json)
-            JsonConvert.DeserializeObject<'T>(x, settings)
+            use ms = new MemoryStream()
+            (   use jsonWriter = new JsonTextWriter(new StreamWriter(ms))
+                serializer.Serialize(jsonWriter, value, typeof<'T>))
+            ms.ToArray()
+        member __.Decode(json : byte[]) =
+            use ms = new MemoryStream(json)
+            use jsonReader = new JsonTextReader(new StreamReader(ms))
+            serializer.Deserialize<'T>(jsonReader)
 
 /// Generates an event sum encoder using Newtonsoft.Json for individual event types that serializes to a UTF-8 array buffer
 let generateJsonUtf8UnionCodec<'Union>(settings) =
