d10cd3c3866172e2dd2a78407935592e3bb6b084
Ruben Bartelink <ruben@jet.com>
2018-06-29 08:29:29 +0100

    rename converter to VerbatimUtf8JsonConverter

--- tests/Equinox.Cosmos.Integration/VerbatimUtf8JsonConverterTests.fs
+++ tests/Equinox.Cosmos.Integration/VerbatimUtf8JsonConverterTests.fs
@@ -1,35 +1,33 @@
-﻿module Foldunk.Equinox.ByteArrayConverterTests
+﻿module Equinox.Cosmos.Integration.VerbatimUtf8JsonConverterTests
 
 open Newtonsoft.Json
 open Swensen.Unquote
 open System
 open Xunit
 
-module Fixtures =
-    type Embedded = { embed : string }
-
 let serializer = new JsonSerializer()
-
 let inline serialize (x:'t) =
     use sw = new System.IO.StringWriter()
     use w = new JsonTextWriter(sw)
     serializer.Serialize(w,x)
     sw.ToString()
 
+type Embedded = { embed : string }
+type Union = A of Embedded | B of Embedded
+
 [<Fact>]
-let ``ByteArrayConverter serializes properly`` () =
-    use sw = new System.IO.StringWriter()
-    use w = new JsonTextWriter(sw)
-    let blob = serialize ({ embed = "\"" } : Fixtures.Embedded) |> System.Text.Encoding.UTF8.GetBytes
+let ``VerbatimUtf8JsonConverter serializes properly`` () =
+    let unionEncoder = Equinox.UnionCodec.generateJsonUtf8UnionCodec<_>(JsonSerializerSettings())
+    let encoded = unionEncoder.Encode(A { embed = "\"" })
     let e : Equinox.Cosmos.EquinoxEvent =
         {   id = null
             s = null
             k  = null
             ts = DateTimeOffset.MinValue
             sn = 0L
-            et = null
 
-            d = blob
+            et = encoded.CaseName
+            d = encoded.Payload
 
             md = null }
     let res = serialize e
