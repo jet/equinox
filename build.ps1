87d296978f8569443cea50957a2efa123cb1f693
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2018-11-01 22:45:40 +0000

    bring ps

--- build.ps1
+++ build.ps1
@@ -1,15 +1,22 @@
 param(
 	[string] $verbosity="m",
-	[switch][bool] $skipEs=$false,
-	[string] $additionalMsBuildArgs="/nowarn:FS0988"
+	[Alias("se")][switch][bool] $skipEs=$false,
+	[string] $additionalMsBuildArgs
 )
 
-$args=@("/v:$verbosity","/fl","/bl","/p:skipes=$skipEs",$additionalMsBuildArgs)
+$args=@("/v:$verbosity","/fl","/bl",$additionalMsBuildArgs)
+
+function warn ($msg) { Write-Host "$msg" -BackgroundColor DarkGreen }
+
+# Yes, this leaves the value set on exit, but I want to keep the script legible
+if ($skipEs) { warn "Skipping EventStore tests" }
+
+$env:EQUINOX_INTEGRATION_SKIP_EVENTSTORE=[string]$skipEs
 
 Write-Host "dotnet msbuild $args"
 . dotnet msbuild build.proj @args
 
-if( $LASTEXITCODE -gt 0) {
-	Write-Host "$message" -BackgroundColor DarkGreen "open msbuild.log for error info or rebuild with -v n/d/diag for more detail, or open msbuild.binlog using https://github.com/KirillOsenkov/MSBuildStructuredLog/releases/download/v2.0.40/MSBuildStructuredLogSetup.exe"
+if( $LASTEXITCODE -ne 0) {
+	warn "open msbuild.log for error info or rebuild with -v n/d/diag for more detail, or open msbuild.binlog using https://github.com/KirillOsenkov/MSBuildStructuredLog/releases/download/v2.0.40/MSBuildStructuredLogSetup.exe"
 	exit $LASTEXITCODE
 }
