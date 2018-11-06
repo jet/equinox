be85119d399a712b66e2d51022dfa7c167919045
Ruben Bartelink <ruben@bartelink.com>
2018-11-06 15:54:23 +0000

    Add base Web sample app

---
+++ samples/Store/Web/Controllers/FavoritesController.fs
@@ -0,0 +1,33 @@
+﻿namespace Web.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Domain
+open Web
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type FavoritesController(service : Backend.Favorites.Service) =
+    inherit ControllerBase()
+
+    [<HttpGet("{clientId}")>]
+    member __.Get(clientId : ClientId, ct) = ctask ct {
+        let! res = service.Read(clientId)
+        return ActionResult<_> res
+    }
+
+    //[<HttpGet("{id}")>]
+    //member this.Get(id:int) =
+    //    let value = "value"
+    //    ActionResult<string>(value)
+
+    //[<HttpPost>]
+    //member this.Post([<FromBody>] value:string) =
+    //    ()
+
+    //[<HttpPut("{id}")>]
+    //member this.Put(id:int, [<FromBody>] value:string ) =
+    //    ()
+
+    //[<HttpDelete("{id}")>]
+    //member this.Delete(id:int) =
+    //    ()
