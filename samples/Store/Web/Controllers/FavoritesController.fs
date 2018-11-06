3673b4c20d2f6d77c65fa0a173f30d0aa1b567e2
Ruben Bartelink <ruben@bartelink.com>
2018-11-06 16:39:22 +0000

    Trust aspnetcore to handle cancellation token propagation

--- samples/Store/Web/Controllers/FavoritesController.fs
+++ samples/Store/Web/Controllers/FavoritesController.fs
@@ -10,18 +10,18 @@ type FavoritesController(service : Backend.Favorites.Service) =
     inherit ControllerBase()
 
     [<HttpGet("{clientId}")>]
-    member __.Get(clientId : ClientId, ct) = ctask ct {
+    member __.Get(clientId : ClientId) = async {
         let! res = service.Read(clientId)
         return ActionResult<_> res
     }
 
     [<HttpPost("{clientId}")>]
-    member __.Favorite(clientId : ClientId, [<FromBody>]skuIds : SkuId[], ct) = ctask ct {
+    member __.Favorite(clientId : ClientId, [<FromBody>]skuIds : SkuId[]) = async {
         let effectiveDate = DateTimeOffset.UtcNow
         return! service.Execute clientId <| Favorites.Command.Favorite(effectiveDate, List.ofArray skuIds)
     }
 
     [<HttpDelete("{clientId}/{skuId}")>]
-    member __.Unfavorite(clientId : ClientId, skuId : SkuId, ct) = ctask ct {
+    member __.Unfavorite(clientId : ClientId, skuId : SkuId) = async {
         return! service.Execute clientId <| Favorites.Command.Unfavorite skuId
     }
