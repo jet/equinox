61a943d072e7160b6254c37842ab124d71b84c22
Ruben Bartelink <ruben@bartelink.com>
2018-11-06 15:54:23 +0000

    Add base Web sample app

---
+++ samples/Store/Web/Infrastructure.fs
@@ -0,0 +1,39 @@
+﻿namespace Web
+
+[<AutoOpen>]
+module Builders =
+    open System.Threading
+    open System.Threading.Tasks
+
+    [<AbstractClass>]
+    type AsyncBuilderAbstract() =
+        member __.Zero() = async.Zero()
+        member __.Return t = async.Return t
+        member __.ReturnFrom t = async.ReturnFrom t
+        member __.Bind(f,g) = async.Bind(f,g)
+        member __.Combine(f,g) = async.Combine(f,g)
+        member __.Delay f = async.Delay f
+        member __.While(c,b) = async.While(c,b)
+        member __.For(xs,b) = async.For(xs,b)
+        member __.TryWith(b,e) = async.TryWith(b,e)
+
+    type TaskBuilder(?ct : CancellationToken) =
+        inherit AsyncBuilderAbstract()
+        member __.Run f : Task<'T> = Async.StartAsTask(f, ?cancellationToken = ct)
+
+    type UnitTaskBuilder() =
+        inherit AsyncBuilderAbstract()
+        member __.Run f : Task = Async.StartAsTask f :> _
+
+    /// Async builder variation that automatically runs top-level expression as task
+    let task = new TaskBuilder()
+
+    /// Async builder variation that automatically runs top-level expression as untyped task
+    let utask = new UnitTaskBuilder()
+
+    /// <summary>Async builder variation that automatically runs top-level expression as task</summary>
+    /// <example>
+    ///     let ct = new CancellationToken()
+    ///     let _ = ctask ct { return 42 }
+    /// </example>
+    let ctask ct = new TaskBuilder(ct)
