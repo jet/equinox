a1f2c790e0871a4d1ec4639d50cab6b6b0fe286b
Ruben Bartelink <ruben@bartelink.com>
2018-11-19 11:50:35 +0000

    Add Better json diffing

--- tests/Equinox.Cosmos.Integration/CosmosEventsIntegration.fs
+++ tests/Equinox.Cosmos.Integration/CosmosEventsIntegration.fs
@@ -4,16 +4,21 @@ open Equinox.Cosmos.Integration.Infrastructure
 open Equinox.Cosmos
 open Swensen.Unquote
 open Serilog
+open System
+open System.Text
 
 #nowarn "1182" // From hereon in, we may have some 'unused' privates (the tests)
 
-open System
+open Newtonsoft.Json.Linq
+
+module Null =
+    let defaultValue d x = if x = null then d else x
 
 type EventData = { eventType:string; data: byte[] } with
     interface Store.IEvent with
         member __.EventType = __.eventType
         member __.Data = __.data
-        member __.Meta = System.Text.Encoding.UTF8.GetBytes("{\"m\":\"m\"}")
+        member __.Meta = Encoding.UTF8.GetBytes("{\"m\":\"m\"}")
     static member Create(eventType,?json) : Store.IEvent =
         {   eventType = eventType
             data = System.Text.Encoding.UTF8.GetBytes(defaultArg json "{\"d\":\"d\"}") } :> _
@@ -37,7 +42,15 @@ type Tests(testOutputHelper) =
         test <@ Events.AppendResult.Ok 1L = res @>
     }
 
-    let blobEquals (x:byte[]) (y:byte[]) = System.Linq.Enumerable.SequenceEqual(x,y)
+    let blobEquals (x: byte[]) (y: byte[]) = System.Linq.Enumerable.SequenceEqual(x,y)
+    let stringOfUtf8 (x: byte[]) = Encoding.UTF8.GetString(x)
+    let xmlDiff (x: string) (y: string) =
+        match JsonDiffPatchDotNet.JsonDiffPatch().Diff(JToken.Parse x,JToken.Parse y) with
+        | null -> ""
+        | d -> string d
+    let testUtf8JsonEquals i x y =
+        let sx,sy = stringOfUtf8 x, stringOfUtf8 y
+        test <@ ignore i; blobEquals x y && "" = xmlDiff sx sy @>
 
     [<AutoData(SkipIfRequestedViaEnvironmentVariable="EQUINOX_INTEGRATION_SKIP_COSMOS")>]
     let get (TestDbCollStream (dbId,collId,sid)) = Async.RunSynchronously <| async {
@@ -52,9 +65,12 @@ type Tests(testOutputHelper) =
         let! res = Events.get ctx sid sn events.Length
         test <@ res.Length = events.Length @>
 
+        test <@ List.init 1 int64 = [for r in res -> r.Index ] @>
+
         test <@ [for e in events -> e.EventType] = [ for r in res -> r.EventType ] @>
 
-        test <@ List.forall2 blobEquals [for e in events -> e.Data] [ for r in res -> r.Data ] @>
+        for i,x,y in Seq.mapi2 (fun i x y -> i,x,y) [for e in events -> e.Data] [ for r in res -> r.Data ] do
+            testUtf8JsonEquals i x y
     }
 
     [<AutoData(SkipIfRequestedViaEnvironmentVariable="EQUINOX_INTEGRATION_SKIP_COSMOS")>]
@@ -70,7 +86,10 @@ type Tests(testOutputHelper) =
         let! res = Events.getAll ctx sid sn events.Length
         test <@ res.Length = events.Length @>
 
+        test <@ List.init 1 int64 = [for r in res -> r.Index ] @>
+
         test <@ [for e in events -> e.EventType] = [ for r in res -> r.EventType ] @>
 
-        test <@ List.forall2 blobEquals [for e in events -> e.Data] [ for r in res -> r.Data ] @>
+        for i,x,y in Seq.mapi2 (fun i x y -> i,x,y) [for e in events -> e.Data] [ for r in res -> r.Data ] do
+            testUtf8JsonEquals i x y
     }
