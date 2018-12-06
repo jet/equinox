ddce4b374401e6b0779883fc5b9cc3bd90c59173
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2018-12-06 08:37:17 +0000

    Add HttpClient helpers

--- cli/Equinox.Cli/Infrastructure/Infrastructure.fs
+++ cli/Equinox.Cli/Infrastructure/Infrastructure.fs
@@ -31,6 +31,12 @@ type Exception with
         Unchecked.defaultof<_>
 
 type Async with
+    /// <summary>
+    ///     Raises an exception using Async's continuation mechanism directly.
+    /// </summary>
+    /// <param name="exn">Exception to be raised.</param>
+    static member Raise (exn : #exn) = Async.FromContinuations(fun (_,ec,_) -> ec exn)
+
 #if NET461
     static member Choice<'T>(workflows : seq<Async<'T option>>) : Async<'T option> = async {
         try
@@ -112,3 +118,130 @@ type StringBuilder with
         let instance = StringBuilder() // TOCONSIDER PooledStringBuilder.GetInstance()
         builder instance
         instance.ToString()
+
+[<AutoOpen>]
+module HttpHelpers =
+
+    open System.Net
+    open System.Net.Http
+    open System.Runtime.Serialization
+
+    /// Operations on System.Net.HttpRequestMessage
+    module HttpReq =
+
+        /// Creates an HTTP GET request.
+        let inline create () = new HttpRequestMessage()
+
+        /// Assigns a path to an HTTP request.
+        let inline withUri (u : Uri) (req : HttpRequestMessage) =
+            req.RequestUri <- u
+            req
+
+        /// Assigns a path to an HTTP request.
+        let inline withPath (p : string) (req : HttpRequestMessage) =
+            req |> withUri (Uri(p, UriKind.Relative))
+
+        /// Assigns a path to a Http request using printf-like formatting.
+        let inline withPathf fmt =
+            Printf.ksprintf withPath fmt
+
+    type HttpContent with
+        member c.ReadAsString() = async {
+            match c with
+            | null -> return null
+            | c -> return! c.ReadAsStringAsync() |> Async.AwaitTaskCorrect
+        }
+
+        // only intended for logging under control of InvalidHttpResponseException, hence the esoteric name
+        member internal c.ReadAsStringDiapered() = async {
+            try return! c.ReadAsString()
+            with :? ObjectDisposedException -> return "<HttpContent:ObjectDisposedException>"
+        }
+
+    type HttpClient with
+        /// <summary>
+        ///     Drop-in replacement for HttpClient.SendAsync which addresses known timeout issues
+        /// </summary>
+        /// <param name="msg">HttpRequestMessage to be submitted.</param>
+        member client.Send(msg : HttpRequestMessage) = async {
+            let! ct = Async.CancellationToken
+            try return! client.SendAsync(msg, ct) |> Async.AwaitTaskCorrect
+            // address https://github.com/dotnet/corefx/issues/20296
+            with :? System.Threading.Tasks.TaskCanceledException ->
+                let message =
+                    match client.BaseAddress with
+                    | null -> "HTTP request timeout"
+                    | baseAddr -> sprintf "HTTP request timeout [%O]" baseAddr
+
+                return! Async.Raise(TimeoutException message)
+        }
+
+    /// Exception indicating an unexpected response received by an Http Client
+    type InvalidHttpResponseException =
+        inherit Exception
+
+        // TODO: include headers
+        val private userMessage : string
+        val private requestMethod : string
+        val RequestUri : Uri
+        val RequestBody : string
+        val StatusCode : HttpStatusCode
+        val ReasonPhrase : string
+        val ResponseBody : string
+
+        member __.RequestMethod = new HttpMethod(__.requestMethod)
+
+        private new (userMessage : string, requestMethod : HttpMethod, requestUri : Uri, requestBody : string,
+                       statusCode : HttpStatusCode, reasonPhrase : string, responseBody : string,
+                       ?innerException : exn) =
+            {
+                inherit Exception(message = null, innerException = defaultArg innerException null) ; userMessage = userMessage ;
+                requestMethod = string requestMethod ; RequestUri = requestUri ; RequestBody = requestBody ;
+                StatusCode = statusCode ; ReasonPhrase = reasonPhrase ; ResponseBody = responseBody
+            }
+
+        override e.Message =
+            StringBuilder.Build(fun sb ->
+                sb.Appendfn "%s %O RequestUri=%O HttpStatusCode=%O" e.userMessage e.RequestMethod e.RequestUri e.StatusCode
+                let getBodyString str = if String.IsNullOrWhiteSpace str then "<null>" else str
+                sb.Appendfn "RequestBody=%s" (getBodyString e.RequestBody)
+                sb.Appendfn "ResponseBody=%s" (getBodyString e.ResponseBody))
+
+        interface ISerializable with
+            member e.GetObjectData(si : SerializationInfo, sc : StreamingContext) =
+                let add name (value:obj) = si.AddValue(name, value)
+                base.GetObjectData(si, sc) ; add "userMessage" e.userMessage ;
+                add "requestUri" e.RequestUri ; add "requestMethod" e.requestMethod ; add "requestBody" e.RequestBody
+                add "statusCode" e.StatusCode ; add "reasonPhrase" e.ReasonPhrase ; add "responseBody" e.ResponseBody
+
+        new (si : SerializationInfo, sc : StreamingContext) =
+            let get name = si.GetValue(name, typeof<'a>) :?> 'a
+            {
+                inherit Exception(si, sc) ; userMessage = get "userMessage" ;
+                RequestUri = get "requestUri" ; requestMethod = get "requestMethod" ; RequestBody = get "requestBody" ;
+                StatusCode = get "statusCode" ; ReasonPhrase = get "reasonPhrase" ; ResponseBody = get "responseBody"
+            }
+
+        static member Create(userMessage : string, response : HttpResponseMessage, ?innerException : exn) = async {
+            let request = response.RequestMessage
+            let! responseBodyC = response.Content.ReadAsStringDiapered() |> Async.StartChild
+            let! requestBody = request.Content.ReadAsStringDiapered()
+            let! responseBody = responseBodyC
+            return
+                new InvalidHttpResponseException(
+                    userMessage, request.Method, request.RequestUri, requestBody,
+                    response.StatusCode, response.ReasonPhrase, responseBody,
+                    ?innerException = innerException)
+        }
+
+        static member Create(response : HttpResponseMessage, ?innerException : exn) =
+            InvalidHttpResponseException.Create("HTTP request yielded unexpected response.", response, ?innerException = innerException)
+
+    type HttpResponseMessage with
+
+        /// Raises an <c>InvalidHttpResponseException</c> if the response status code does not match expected value.
+        member response.EnsureStatusCode(expectedStatusCode : HttpStatusCode) = async {
+            if response.StatusCode <> expectedStatusCode then
+                let! exn = InvalidHttpResponseException.Create("Http request yielded unanticipated HTTP Result.", response)
+                do raise exn
+        }
