5f24e78486926b25e088dc51e9f5d1a827aafb8f
Ruben Bartelink <ruben@bartelink.com>
2018-12-12 13:59:34 +0000

    Stop caching failures

--- src/Equinox.EventStore/Infrastructure.fs
+++ src/Equinox.EventStore/Infrastructure.fs
@@ -122,22 +122,32 @@ type Stopwatch =
 type AsyncLazy<'T>(workflow : Async<'T>) =
     let task = lazy(Async.StartAsTask workflow)
     member __.AwaitValue() = Async.AwaitTaskCorrect task.Value
+    member internal __.PeekInternalTask = task
 
 /// Generic async lazy caching implementation that admits expiration/recomputation semantics
+/// If `workflow` fails, all readers entering while the load/refresh is in progress will share the failure
 type AsyncCacheCell<'T>(workflow : Async<'T>, isExpired : 'T -> bool) =
     let mutable currentCell = AsyncLazy workflow
+    let update cell = async {
+        // avoid unneccessary recomputation in cases where competing threads detect expiry;
+        // the first write attempt wins, and everybody else reads off that value.
+        let _ = System.Threading.Interlocked.CompareExchange(&currentCell, AsyncLazy workflow, cell)
+        return! currentCell.AwaitValue()
+    }
 
     /// Gets or asynchronously recomputes a cached value depending on expiry and availability
     member __.AwaitValue() = async {
         let cell = currentCell
-        let! current = cell.AwaitValue()
-        if isExpired current then
-            // avoid unneccessary recomputation in cases where competing threads detect expiry;
-            // the first write attempt wins, and everybody else reads off that value.
-            let _ = System.Threading.Interlocked.CompareExchange(&currentCell, AsyncLazy workflow, cell)
-            return! currentCell.AwaitValue()
+        let currentState = cell.PeekInternalTask
+        // If the last attempt completed, but failed, we need to treat it as expired; for TMI on this, see https://stackoverflow.com/a/33946166/11635
+        if currentState.IsValueCreated && currentState.Value.IsCompleted && currentState.Value.Status <> System.Threading.Tasks.TaskStatus.RanToCompletion then
+            return! update cell
         else
-            return current
+            let! current = cell.AwaitValue()
+            if isExpired current then
+                return! update cell
+            else
+                return current
     }
     
 [<RequireQualifiedAccess>]
