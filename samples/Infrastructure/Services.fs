0438e70875b66643053383f1c797b99a5f050dee
Benney Au <benney.au@prospa.com>
2018-12-26 19:55:47 +1100

    fix parameter name in sample project

--- samples/Infrastructure/Services.fs
+++ samples/Infrastructure/Services.fs
@@ -18,8 +18,8 @@ type StreamResolver(storage) =
         | Storage.StorageConfig.Es (gateway, cache, unfolds) ->
             let accessStrategy = if unfolds then Equinox.EventStore.AccessStrategy.RollingSnapshots snapshot |> Some else None
             Equinox.EventStore.GesResolver<'event,'state>(gateway, codec, fold, initial, ?access = accessStrategy, ?caching = cache).Resolve
-        | Storage.StorageConfig.Cosmos (gateway, cache, unfolds, databaseId, connectionId) ->
-            let store = Equinox.Cosmos.EqxStore(gateway, Equinox.Cosmos.EqxCollections(databaseId, connectionId))
+        | Storage.StorageConfig.Cosmos (gateway, cache, unfolds, databaseId, collectionId) ->
+            let store = Equinox.Cosmos.EqxStore(gateway, Equinox.Cosmos.EqxCollections(databaseId, collectionId))
             let accessStrategy = if unfolds then Equinox.Cosmos.AccessStrategy.Snapshot snapshot |> Some else None
             Equinox.Cosmos.EqxResolver<'event,'state>(store, codec, fold, initial, ?access = accessStrategy, ?caching = cache).Resolve
 
