36eb52c09cd0fe7ff9c33e054eb3a9e90ac698a9
Ming Luo <ming.luo10@gmail.com>
2019-01-17 01:52:40 +0000

    Add Event parsing

--- src/Equinox.Cosmos.Projection/Projection.fs
+++ src/Equinox.Cosmos.Projection/Projection.fs
@@ -10,6 +10,35 @@ open Serilog
 open System
 open System.Collections.Generic
 
+[<RequireQualifiedAccess>]
+module Parse =
+    type Document with
+        member document.Cast<'T>() =
+            let tmp = new Document()
+            tmp.SetPropertyValue("content", document)
+            tmp.GetPropertyValue<'T>("content")
+    /// Determines whether this document represents an index page [and hence should not be expected to contain any events]
+    let isIndex (d : Document) = d.Id = "-1"
+    type IEvent =
+        inherit Equinox.Cosmos.Store.IIndexedEvent
+            abstract member Stream : string
+            abstract member TimeStamp : DateTimeOffset
+    /// Infers whether the document represents a valid Event-Batch
+    let enumEvents (d : Document) = seq {
+        if not (isIndex d)
+           && d.GetPropertyValue("p") <> null && d.GetPropertyValue("i") <> null
+           && d.GetPropertyValue("n") <> null && d.GetPropertyValue("e") <> null then
+            let batch = d.Cast<Equinox.Cosmos.Store.Batch>()
+            yield! batch.e |> Seq.mapi (fun offset x ->
+                { new IEvent with
+                      member __.Index = batch.i + int64 offset
+                      member __.IsUnfold = false
+                      member __.EventType = x.c
+                      member __.Data = x.d
+                      member __.Meta = x.m
+                      member __.TimeStamp = x.t
+                      member __.Stream = batch.p } ) }
+
 [<AutoOpen>]
 module Wrappers =
     type IChangeFeedObserver with
