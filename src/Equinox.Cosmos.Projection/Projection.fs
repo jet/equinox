f5b7006daa333f687b8105f1f34f739c1b4c5a16
Ruben Bartelink <ruben@bartelink.com>
2019-01-17 01:53:47 +0000

    Add event parsing to project command

--- src/Equinox.Cosmos.Projection/Projection.fs
+++ src/Equinox.Cosmos.Projection/Projection.fs
@@ -64,18 +64,21 @@ type CosmosCollection = { database: string; collection: string }
 type ChangeFeedProcessor =
     static member Start
         (   log : ILogger, endpoint : Uri, accountKey : string, connectionPolicy : ConnectionPolicy, source : CosmosCollection,
+            /// The aux, non-partitioned collection holding the partition leases.
             // Aux coll should always read from the write region to keep the number of write conflicts to minimum when the sdk
             // updates the leases. Since the non-write region(s) might lag behind due to us using non-strong consistency, during
             // failover we are likely to reprocess some messages, but that's okay since we are idempotent.
-            // The aux, non-partitioned collection holding the partition leases.
             aux : CosmosCollection,
             changeFeedObserver : IChangeFeedObserver,
             ?leaseOwnerId : string,
             /// Identifier to disambiguate multiple independent feed processor positions
             ?leasePrefix : string,
-            /// (Only if this is the first time this leasePrefix is presnted) Request starting of projection from this position (as opposed to projecting all events from start beforehand). Default: false.
+            /// (NB Only applies if this is the first time this leasePrefix is presented)
+            /// Specify `true` to request starting of projection from the present write position.
+            /// Default: false (projecting all events from start beforehand)
             ?forceSkipExistingEvents : bool,
-            /// Limit on items to take in a batch when querying for changes. Default 1000
+            /// Limit on items to take in a batch when querying for changes (in addition to 4MB response size limit). Default 1000
+            /// NB While a larger limit may deliver better throughput, it's important to balance that .concern with the risk of throttling
             ?cfBatchSize : int, 
             /// Frequency to check for partitions without a processor. Default 1s
             ?leaseAcquireInterval : TimeSpan,
@@ -110,7 +113,7 @@ type ChangeFeedProcessor =
                 LeaseAcquireInterval = leaseAcquireInterval, LeaseExpirationInterval = leaseTtl, LeaseRenewInterval = leaseRenewInterval,
                 FeedPollDelay = feedPollDelay)
 
-        leasePrefix |> Option.iter (fun lp -> feedProcessorOptions.LeasePrefix <- lp + "/")
+        leasePrefix |> Option.iter (fun lp -> feedProcessorOptions.LeasePrefix <- lp + ":")
 
         let factory = IChangeFeedObserverFactory.FromFunction (fun () -> changeFeedObserver)
        
@@ -146,10 +149,10 @@ type ChangeFeedProcessor =
 
         return processor }
     static member private mkLeaseOwnerIdForProcess() =
-        // If k>1 processes share owner id, then they will compete for same partitions.
+        // If k>1 processes share an owner id, then they will compete for same partitions.
         // In that scenario, redundant processing happen on assigned partitions, but checkpoint will process on only 1 consumer.
         // Including the processId should eliminate the possibility that a broken process manager causes k>1 scenario to happen.
-        // The only downside is that upon redeploy, lease ttl would have to be observed before a consumer can pick it up.
+        // The only downside is that upon redeploy, lease experation / TTL would have to be observed before a consumer can pick it up.
         let processName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name
         let processId = System.Diagnostics.Process.GetCurrentProcess().Id
         let hostName = System.Environment.MachineName
