c451e99ab4bb4d4b5e403f53eb169a113b27fb4c
Michael Liao <michael.liao28@gmail.com>
2019-02-11 12:26:11 -0500

    set consumer number in test back to 10

--- tests/Equinox.Projection.Kafka.Integration/KafkaIntegration.fs
+++ tests/Equinox.Projection.Kafka.Integration/KafkaIntegration.fs
@@ -117,7 +117,7 @@ type T1(testOutputHelper) =
 
     let [<FactIfBroker>] ``ConfluentKafka producer-consumer basic roundtrip`` () = async {
         let numProducers = 10
-        let numConsumers = 1
+        let numConsumers = 10
         let messagesPerProducer = 1000
 
         let topic = newId() // dev kafka topics are created and truncated automatically
