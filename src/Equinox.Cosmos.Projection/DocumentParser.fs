b68c33b14c3f1d626b70d06728a4c8999b1efca1
Ruben Bartelink <ruben@bartelink.com>
2019-05-05 23:58:48 +0100

    Split out Equinox.Projection.Codec (#125)
    * Calve Equinox.Projection.Codec from Equinox.Projection
    * Add + use Equinox.Projection.StreamItem

--- src/Equinox.Cosmos.Projection/DocumentParser.fs
+++ src/Equinox.Cosmos.Projection/DocumentParser.fs
@@ -9,28 +9,25 @@ module DocumentParser =
             let tmp = new Document()
             tmp.SetPropertyValue("content", document)
             tmp.GetPropertyValue<'T>("content")
-    type IEvent =
-        inherit Equinox.Codec.Core.IIndexedEvent<byte[]>
-            abstract member Stream : string
     /// Sanity check to determine whether the Document represents an `Equinox.Cosmos` >= 1.0 based batch
     let isEquinoxBatch (d : Document) = 
         d.GetPropertyValue "p" <> null && d.GetPropertyValue "i" <> null
         && d.GetPropertyValue "n" <> null && d.GetPropertyValue "e" <> null
+    /// Maps fields in an Event within an Equinox.Cosmos V1+ Event (in a Batch or Tip) to the interface defined by the default Codec
+    let (|CodecEvent|) (x: Equinox.Cosmos.Store.Event) =
+        { new Equinox.Codec.IEvent<_> with
+            member __.EventType = x.c
+            member __.Data = x.d
+            member __.Meta = x.m
+            member __.Timestamp = x.t }
+
     /// Enumerates the events represented within a batch
     // NOTE until `tip-isa-batch` gets merged, this causes a null-traversal of `-1`-index pages which presently do not contain data
     // This is intentional in the name of forward compatibility for projectors - enabling us to upgrade the data format without necessitating
     //   updates of all projectors (even if there can potentially be significant at-least-once-ness to the delivery)
     let enumEquinoxCosmosEvents (batch : Equinox.Cosmos.Store.Batch) =
-        batch.e |> Seq.mapi (fun offset x ->
-            { new IEvent with
-                  member __.Index = batch.i + int64 offset
-                  member __.IsUnfold = false
-                  member __.EventType = x.c
-                  member __.Data = x.d
-                  member __.Meta = x.m
-                  member __.Timestamp = x.t
-                  member __.Stream = batch.p } )
+        batch.e |> Seq.mapi (fun offset (CodecEvent x) -> { stream = batch.p; index = batch.i + int64 offset; event = x } : Equinox.Projection.StreamItem)
     /// Collects all events with a Document [typically obtained via the CosmosDb ChangeFeed] that potentially represents an Equinox.Cosmos event-batch
-    let enumEvents (d : Document) : IEvent seq =
+    let enumEvents (d : Document) : Equinox.Projection.StreamItem seq =
         if isEquinoxBatch d then d.Cast<Equinox.Cosmos.Store.Batch>() |> enumEquinoxCosmosEvents
         else Seq.empty
---
+++ src/Equinox.Projection/Projection.fs
@@ -0,0 +1,4 @@
+﻿namespace Equinox.Projection
+
+/// Item from a reader as supplied to the `IIngester`
+type [<NoComparison>] StreamItem = { stream: string; index: int64; event: Equinox.Codec.IEvent<byte[]> }
