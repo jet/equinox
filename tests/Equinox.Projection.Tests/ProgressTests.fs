883e331ef4e306e0b3b6f23d17bc2fb352f33bf9
Ruben Bartelink <ruben@bartelink.com>
2019-05-06 07:15:29 +0100

    Projection State / Pipeline + Tests (#128)

---
+++ tests/Equinox.Projection.Tests/ProgressTests.fs
@@ -0,0 +1,47 @@
+﻿module ProgressTests
+
+open Equinox.Projection
+open Swensen.Unquote
+open System.Collections.Generic
+open Xunit
+
+let mkDictionary xs = Dictionary<string,int64>(dict xs)
+
+let [<Fact>] ``Empty has zero streams pending or progress to write`` () =
+    let sut = Progress.State<_>()
+    let queue = sut.InScheduledOrder(fun _ -> 0)
+    test <@ Seq.isEmpty queue @>
+
+let [<Fact>] ``Can add multiple batches with overlapping streams`` () =
+    let sut = Progress.State<_>()
+    let noBatchesComplete () = failwith "No bathes should complete"
+    sut.AppendBatch(noBatchesComplete, mkDictionary ["a",1L; "b",2L])
+    sut.AppendBatch(noBatchesComplete, mkDictionary ["b",2L; "c",3L])
+
+let [<Fact>] ``Marking Progress removes batches and triggers the callbacks`` () =
+    let sut = Progress.State<_>()
+    let mutable callbacks = 0
+    let complete () = callbacks <- callbacks + 1
+    sut.AppendBatch(complete, mkDictionary ["a",1L; "b",2L])
+    sut.MarkStreamProgress("a",1L) |> ignore
+    sut.MarkStreamProgress("b",3L) |> ignore
+    1 =! callbacks
+
+let [<Fact>] ``Empty batches get removed immediately`` () =
+    let sut = Progress.State<_>()
+    let mutable callbacks = 0
+    let complete () = callbacks <- callbacks + 1
+    sut.AppendBatch(complete, mkDictionary [||])
+    sut.AppendBatch(complete, mkDictionary [||])
+    2 =! callbacks
+
+let [<Fact>] ``Marking progress is not persistent`` () =
+    let sut = Progress.State<_>()
+    let mutable callbacks = 0
+    let complete () = callbacks <- callbacks + 1
+    sut.AppendBatch(complete, mkDictionary ["a",1L])
+    sut.MarkStreamProgress("a",2L) |> ignore
+    sut.AppendBatch(complete, mkDictionary ["a",1L; "b",2L])
+    1 =! callbacks
+
+// TODO: lots more coverage of newer functionality - the above were written very early into the exercise
---
+++ tests/Equinox.Projection.Tests/StreamStateTests.fs
@@ -0,0 +1,78 @@
+﻿module CosmosIngesterTests
+
+open Equinox.Projection.State
+open Swensen.Unquote
+open Xunit
+
+let canonicalTime = System.DateTimeOffset.UtcNow
+let mk p c : Span = { index = p; events = [| for x in 0..c-1 -> Equinox.Codec.Core.EventData.Create(p + int64 x |> string, null, timestamp=canonicalTime) |] }
+let mergeSpans = StreamState.Span.merge
+let trimSpans = StreamState.Span.trim
+
+let [<Fact>] ``nothing`` () =
+    let r = mergeSpans 0L [ mk 0L 0; mk 0L 0 ]
+    r =! null
+
+let [<Fact>] ``synced`` () =
+    let r = mergeSpans 1L [ mk 0L 1; mk 0L 0 ]
+    r =! null
+
+let [<Fact>] ``no overlap`` () =
+    let r = mergeSpans 0L [ mk 0L 1; mk 2L 2 ]
+    r =! [| mk 0L 1; mk 2L 2 |]
+
+let [<Fact>] ``overlap`` () =
+    let r = mergeSpans 0L [ mk 0L 1; mk 0L 2 ]
+    r =! [| mk 0L 2 |]
+
+let [<Fact>] ``remove nulls`` () =
+    let r = mergeSpans 1L [ mk 0L 1; mk 0L 2 ]
+    r =! [| mk 1L 1 |]
+
+let [<Fact>] ``adjacent`` () =
+    let r = mergeSpans 0L [ mk 0L 1; mk 1L 2 ]
+    r =! [| mk 0L 3 |]
+
+let [<Fact>] ``adjacent to min`` () =
+    let r = List.map (trimSpans 2L) [ mk 0L 1; mk 1L 2 ]
+    r =! [ mk 2L 0; mk 2L 1 ]
+
+let [<Fact>] ``adjacent to min merge`` () =
+    let r = mergeSpans 2L [ mk 0L 1; mk 1L 2 ]
+    r =! [| mk 2L 1 |]
+
+let [<Fact>] ``adjacent to min no overlap`` () =
+    let r = mergeSpans 2L [ mk 0L 1; mk 2L 1 ]
+    r =! [| mk 2L 1|]
+
+let [<Fact>] ``adjacent trim`` () =
+    let r = List.map (trimSpans 1L) [ mk 0L 2; mk 2L 2 ]
+    r =! [ mk 1L 1; mk 2L 2 ]
+
+let [<Fact>] ``adjacent trim merge`` () =
+    let r = mergeSpans 1L [ mk 0L 2; mk 2L 2 ]
+    r =! [| mk 1L 3 |]
+
+let [<Fact>] ``adjacent trim append`` () =
+    let r = List.map (trimSpans 1L) [ mk 0L 2; mk 2L 2; mk 5L 1]
+    r =! [ mk 1L 1; mk 2L 2; mk 5L 1 ]
+
+let [<Fact>] ``adjacent trim append merge`` () =
+    let r = mergeSpans 1L [ mk 0L 2; mk 2L 2; mk 5L 1]
+    r =! [| mk 1L 3; mk 5L 1 |]
+
+let [<Fact>] ``mixed adjacent trim append`` () =
+    let r = List.map (trimSpans 1L) [ mk 0L 2; mk 5L 1; mk 2L 2]
+    r =! [ mk 1L 1; mk 5L 1; mk 2L 2 ]
+
+let [<Fact>] ``mixed adjacent trim append merge`` () =
+    let r = mergeSpans 1L [ mk 0L 2; mk 5L 1; mk 2L 2]
+    r =! [| mk 1L 3; mk 5L 1 |]
+
+let [<Fact>] ``fail`` () =
+    let r = mergeSpans 11614L [ {index=11614L; events=null}; mk 11614L 1 ]
+    r =! [| mk 11614L 1 |]
+
+let [<Fact>] ``fail 2`` () =
+    let r = mergeSpans 11613L [ mk 11614L 1; {index=11614L; events=null} ]
+    r =! [| mk 11614L 1 |]
