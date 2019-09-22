68946f37e8376004f641f1e65c11f4ed91487c7d
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2019-09-22 15:40:18 +0100

    :sparkles: Add `MsSql` connection stuff.

---
+++ src/Equinox.SqlStreamStore.MsSql/Connection.fs
@@ -0,0 +1,21 @@
+﻿namespace Equinox.SqlStreamStore.MsSql
+
+open Equinox
+open Equinox.Store
+open Equinox.SqlStreamStore
+
+type Connector (connectionString: string(*, [<O; D(null)>]?schema: string*), [<O; D(null)>]?readRetryPolicy, [<O; D(null)>]?writeRetryPolicy) =
+    let storeSettings = SqlStreamStore.MsSqlStreamStoreV3Settings(connectionString)
+
+    let store = new SqlStreamStore.MsSqlStreamStoreV3(storeSettings)
+
+    member __.Connect () = async {
+        do! store.CreateSchemaIfNotExists() |> Async.AwaitTask
+
+        return (store :> SqlStreamStore.IStreamStore)
+    }
+
+    member __.Establish () = async {
+        let! conn = __.Connect()
+        return Connection(conn, ?readRetryPolicy=readRetryPolicy, ?writeRetryPolicy=writeRetryPolicy) 
+    }
