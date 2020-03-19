97eb19171c9849b4ddcec4abc7b9257ee752be53
Yaron Librach <ylibrach@gmail.com>
2020-03-19 10:21:01 -0400

    Test compression through Property

--- tests/Equinox.Cosmos.Integration/JsonConverterTests.fs
+++ tests/Equinox.Cosmos.Integration/JsonConverterTests.fs
@@ -20,7 +20,7 @@ type Base64ZipUtf8Tests() =
     let eventCodec = FsCodec.SystemTextJson.Codec.Create<Union>(defaultOptions)
 
     [<Property>]
-    let ``Can read compressed`` value =
+    let ``Can read uncompressed and compressed`` compress value =
         let hasNulls =
             match value with
             | A x | B x when obj.ReferenceEquals(null, x) -> true
@@ -28,31 +28,11 @@ type Base64ZipUtf8Tests() =
         if hasNulls then () else
 
         let encoded = eventCodec.Encode(None,value)
+        let compressor = if compress then JsonCompressedBase64Converter.Compress else id
         let e : Store.Unfold =
             {   i = 42L
                 c = encoded.EventType
-                d = JsonCompressedBase64Converter.Compress encoded.Data
-                m = Unchecked.defaultof<JsonElement>
-                t = DateTimeOffset.MinValue }
-        let ser = FsCodec.SystemTextJson.Serdes.Serialize(e, defaultOptions)
-        let des = FsCodec.SystemTextJson.Serdes.Deserialize<Store.Unfold>(ser, defaultOptions)
-        let d = FsCodec.Core.TimelineEvent.Create(-1L, des.c, des.d)
-        let decoded = eventCodec.TryDecode d |> Option.get
-        test <@ value = decoded @>
-
-    [<Property>]
-    let ``Can read uncompressed`` value =
-        let hasNulls =
-            match value with
-            | A x | B x when obj.ReferenceEquals(null, x) -> true
-            | A { embed = x } | B { embed = x } -> obj.ReferenceEquals(null, x)
-        if hasNulls then () else
-
-        let encoded = eventCodec.Encode(None,value)
-        let e : Store.Unfold =
-            {   i = 42L
-                c = encoded.EventType
-                d = encoded.Data
+                d = compressor encoded.Data
                 m = Unchecked.defaultof<JsonElement>
                 t = DateTimeOffset.MinValue }
         let ser = FsCodec.SystemTextJson.Serdes.Serialize(e, defaultOptions)
