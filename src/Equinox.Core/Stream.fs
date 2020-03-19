a188f69bc02ef5c7015d6e8398d90456f530413f
Yaron Librach <ylibrach@gmail.com>
2020-03-19 11:11:34 -0400

    Tip compression toggle through optional argument (#206)
    * Add optional compress flag to toggle tip compression

--- src/Equinox.Core/Stream.fs
+++ src/Equinox.Core/Stream.fs
@@ -2,15 +2,15 @@
 module Equinox.Core.Stream
 
 /// Represents a specific stream in a ICategory
-type private Stream<'event, 'state, 'streamId, 'context>(category : ICategory<'event, 'state, 'streamId, 'context>, streamId: 'streamId, opt, context) =
+type private Stream<'event, 'state, 'streamId, 'context>(category : ICategory<'event, 'state, 'streamId, 'context>, streamId: 'streamId, opt, context, compress) =
     interface IStream<'event, 'state> with
         member __.Load log =
             category.Load(log, streamId, opt)
 
         member __.TrySync(log: Serilog.ILogger, token: StreamToken, originState: 'state, events: 'event list) =
-            category.TrySync(log, token, originState, events, context)
+            category.TrySync(log, token, originState, events, context, compress)
 
-let create (category : ICategory<'event, 'state, 'streamId, 'context>) streamId opt context : IStream<'event, 'state> = Stream(category, streamId, opt, context) :> _
+let create (category : ICategory<'event, 'state, 'streamId, 'context>) streamId opt context compress : IStream<'event, 'state> = Stream(category, streamId, opt, context, compress) :> _
 
 /// Handles case where some earlier processing has loaded or determined a the state of a stream, allowing us to avoid a read roundtrip
 type private InitializedStream<'event, 'state>(inner : IStream<'event, 'state>, memento : StreamToken * 'state) =
