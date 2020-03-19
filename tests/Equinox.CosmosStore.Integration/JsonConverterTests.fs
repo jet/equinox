a9f5bc3ca933f877717888445e030f00556cfc84
Yaron Librach <ylibrach@gmail.com>
2020-03-19 11:11:34 -0400

    Ported from original by @ylibrach in #206

--- tests/Equinox.CosmosStore.Integration/JsonConverterTests.fs
+++ tests/Equinox.CosmosStore.Integration/JsonConverterTests.fs
@@ -18,31 +18,38 @@ let defaultSettings = FsCodec.NewtonsoftJson.Settings.CreateDefault()
 type Base64ZipUtf8Tests() =
     let eventCodec = FsCodec.NewtonsoftJson.Codec.Create(defaultSettings)
 
-    [<Fact>]
-    let ``serializes, achieving compression`` () =
-        let encoded = eventCodec.Encode(None,A { embed = String('x',5000) })
+    let ser eventType data =
         let e : Core.Unfold =
             {   i = 42L
-                c = encoded.EventType
-                d = encoded.Data
+                c = eventType
+                d = data
                 m = null
                 t = DateTimeOffset.MinValue }
-        let res = JsonConvert.SerializeObject e
-        test <@ res.Contains("\"d\":\"") && res.Length < 128 @>
+        JsonConvert.SerializeObject e
+
+    [<Fact>]
+    let ``serializes, achieving expected compression`` () =
+        let encoded = eventCodec.Encode(None,A { embed = String('x',5000) })
+        let res = ser encoded.EventType (Core.Base64MaybeDeflateUtf8JsonConverter.Compress encoded.Data)
+        test <@ res.Contains("\"d\":\"") && res.Length < 138 @>
 
     [<Property>]
-    let roundtrips value =
-        let encoded = eventCodec.Encode(None,value)
-        let e : Core.Unfold =
-            {   i = 42L
-                c = encoded.EventType
-                d = encoded.Data
-                m = null
-                t = DateTimeOffset.MinValue }
-        let ser = JsonConvert.SerializeObject(e)
-        test <@ ser.Contains("\"d\":\"") @>
-        System.Diagnostics.Trace.WriteLine ser
+    let roundtrips compress value =
+        let encoded = eventCodec.Encode(None, value)
+        let maybeCompressor = if compress then Core.Base64MaybeDeflateUtf8JsonConverter.Compress else id
+        let actualData = maybeCompressor encoded.Data
+        let ser = ser encoded.EventType actualData
+        test <@ if compress then ser.Contains("\"d\":\"")
+                else ser.Contains("\"d\":{") @>
         let des = JsonConvert.DeserializeObject<Core.Unfold>(ser)
         let d = FsCodec.Core.TimelineEvent.Create(-1L, des.c, des.d)
         let decoded = eventCodec.TryDecode d |> Option.get
         test <@ value = decoded @>
+
+    [<Theory; InlineData false; InlineData true>]
+    let handlesNulls compress =
+        let maybeCompressor = if compress then Core.Base64MaybeDeflateUtf8JsonConverter.Compress else id
+        let maybeCompressed = maybeCompressor null
+        let ser = ser "AnEventType" maybeCompressed
+        let des = JsonConvert.DeserializeObject<Core.Unfold>(ser)
+        test <@ null = des.d @>
