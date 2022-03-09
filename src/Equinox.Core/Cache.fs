00793ae502953abab678c57a032b821eda9c6e0a
Ruben Bartelink <ruben@bartelink.com>
2022-03-09 10:04:14 +0000

    Shift to own file

--- src/Equinox.Core/Cache.fs
+++ src/Equinox.Core/Cache.fs
@@ -24,52 +24,6 @@ type ICache =
     abstract member UpdateIfNewer : key: string * options: CacheItemOptions * entry: CacheEntry<'state> -> unit
     abstract member TryGet : key: string -> (StreamToken * 'state) option
 
-module Caching =
-
-    type internal CachingDecorator<'event, 'state, 'context>(inner : ICategory<'event, 'state, string, 'context>, updateCache : string -> StreamToken * 'state -> unit) =
-
-        let cache streamName inner = async {
-            let! tokenAndState = inner
-            updateCache streamName tokenAndState
-            return tokenAndState }
-
-        interface ICategory<'event, 'state, string, 'context> with
-            member _.Load(log, streamName : string, opt) : Async<StreamToken * 'state> =
-                inner.Load(log, streamName, opt) |> cache streamName
-
-            member _.TrySync(log : Serilog.ILogger, streamName, streamToken, state, events : 'event list, context) : Async<SyncResult<'state>> = async {
-                match! inner.TrySync(log, streamName, streamToken, state, events, context) with
-                | SyncResult.Conflict resync -> return SyncResult.Conflict (resync |> cache streamName)
-                | SyncResult.Written (token', state') ->
-                    updateCache streamName (token', state')
-                    return SyncResult.Written (token', state') }
-
-    let applyCacheUpdatesWithSlidingExpiration
-            (cache : ICache)
-            (prefix : string)
-            (slidingExpiration : TimeSpan)
-            (category : ICategory<'event, 'state, string, 'context>)
-            supersedes
-            : ICategory<'event, 'state, string, 'context> =
-        let mkCacheEntry (initialToken : StreamToken, initialState : 'state) = new CacheEntry<'state>(initialToken, initialState, supersedes)
-        let options = CacheItemOptions.RelativeExpiration slidingExpiration
-        let addOrUpdateSlidingExpirationCacheEntry streamName value = cache.UpdateIfNewer(prefix + streamName, options, mkCacheEntry value)
-        CachingDecorator<'event, 'state, 'context>(category, addOrUpdateSlidingExpirationCacheEntry) :> _
-
-    let applyCacheUpdatesWithFixedTimeSpan
-            (cache : ICache)
-            (prefix : string)
-            (lifetime : TimeSpan)
-            (category : ICategory<'event, 'state, string, 'context>)
-            supersedes
-            : ICategory<'event, 'state, string, 'context> =
-        let mkCacheEntry (initialToken : StreamToken, initialState : 'state) = CacheEntry<'state>(initialToken, initialState, supersedes)
-        let addOrUpdateFixedLifetimeCacheEntry streamName value =
-            let expirationPoint = let creationDate = DateTimeOffset.UtcNow in creationDate.Add lifetime
-            let options = CacheItemOptions.AbsoluteExpiration expirationPoint
-            cache.UpdateIfNewer(prefix + streamName, options, mkCacheEntry value)
-        CachingDecorator<'event, 'state, 'context>(category, addOrUpdateFixedLifetimeCacheEntry) :> _
-
 namespace Equinox
 
 open System.Runtime.Caching
