ac3fa327aa8834ae0a580cab359129efff8ea065
Ruben Bartelink <ruben@bartelink.com>
2022-05-18 11:27:59 +0100

    AwaitTaskCorrect sync (no consequential changes)

--- src/Equinox.Core/Infrastructure.fs
+++ src/Equinox.Core/Infrastructure.fs
@@ -9,6 +9,7 @@ open System.Threading.Tasks
 type OAttribute = System.Runtime.InteropServices.OptionalAttribute
 type DAttribute = System.Runtime.InteropServices.DefaultParameterValueAttribute
 
+// http://www.fssnip.net/7Rc/title/AsyncAwaitTaskCorrect pending that being officially packaged somewhere or integrated into FSharp.Core
 type Async with
     /// <summary>
     ///     Gets the result of given task so that in the event of exception
@@ -18,26 +19,32 @@ type Async with
     /// <param name="task">Task to be awaited.</param>
     [<DebuggerStepThrough>]
     static member AwaitTaskCorrect(task : Task<'T>) : Async<'T> =
-        Async.FromContinuations(fun (sc, ec, _) ->
+        Async.FromContinuations(fun (sc, ec, _cc) ->
             task.ContinueWith(fun (t : Task<'T>) ->
                 if t.IsFaulted then
                     let e = t.Exception
-                    if e.InnerExceptions.Count = 1 then ec e.InnerExceptions.[0]
+                    if e.InnerExceptions.Count = 1 then ec e.InnerExceptions[0]
                     else ec e
-                elif t.IsCanceled then ec(new TaskCanceledException())
+                elif t.IsCanceled then ec (TaskCanceledException())
                 else sc t.Result)
             |> ignore)
 
+    /// <summary>
+    ///     Gets the result of given task so that in the event of exception
+    ///     the actual user exception is raised as opposed to being wrapped
+    ///     in a System.AggregateException.
+    /// </summary>
+    /// <param name="task">Task to be awaited.</param>
     [<DebuggerStepThrough>]
     static member AwaitTaskCorrect(task : Task) : Async<unit> =
-        Async.FromContinuations(fun (sc, ec, _) ->
+        Async.FromContinuations(fun (sc, ec, _cc) ->
             task.ContinueWith(fun (task : Task) ->
                 if task.IsFaulted then
                     let e = task.Exception
-                    if e.InnerExceptions.Count = 1 then ec e.InnerExceptions.[0]
+                    if e.InnerExceptions.Count = 1 then ec e.InnerExceptions[0]
                     else ec e
                 elif task.IsCanceled then
-                    ec(TaskCanceledException())
+                    ec (TaskCanceledException())
                 else
                     sc ())
             |> ignore)
