c66cbd3774a1cf54c7e376322093bf536bc49875
Einar Norðfjörð <einar@nordfjord.io>
2022-11-10 19:03:15 -0500

    simplify connector code

--- src/Equinox.MessageDb/MessageDb.fs
+++ src/Equinox.MessageDb/MessageDb.fs
@@ -406,18 +406,16 @@ type MessageDbCategory<'event, 'state, 'context>(resolveInner, empty) =
 
 
 type MessageDbConnector(
-    connectionString : string, ?readOnlyConnectionString : string,
+    connectionString : string, [<O; D(null)>]?readConnectionString : string,
     [<O; D(null)>]?readRetryPolicy, [<O; D(null)>]?writeRetryPolicy) =
-        let readOnlyConnectionString = defaultArg readOnlyConnectionString connectionString
-        member _.Connect() =
-            let connectToDb connectionString ct = task {
-                let conn = new NpgsqlConnection(connectionString)
-                do! conn.OpenAsync(ct)
-                return conn }
-            let writeConnection = connectToDb connectionString
-            let readConnection = connectToDb readOnlyConnectionString
-            MessageDbClient(writeConnection), MessageDbClient(readConnection)
+        let readOnlyConnectionString = defaultArg readConnectionString connectionString
+        let connectToDb connectionString ct = task {
+            let conn = new NpgsqlConnection(connectionString)
+            do! conn.OpenAsync(ct)
+            return conn }
 
         member x.Establish() : MessageDbConnection =
-            let write, read = x.Connect()
-            MessageDbConnection(readConnection = read, writeConnection = write, ?readRetryPolicy = readRetryPolicy, ?writeRetryPolicy = writeRetryPolicy)
+            let writeConnection = MessageDbClient(connectToDb connectionString)
+            let readConnection = MessageDbClient(connectToDb readOnlyConnectionString)
+            MessageDbConnection(readConnection, writeConnection, ?readRetryPolicy = readRetryPolicy, ?writeRetryPolicy = writeRetryPolicy)
+
