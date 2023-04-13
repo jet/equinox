3042d59c84153e2233bdb9fc2d57be939f781b32
Ruben Bartelink <ruben@bartelink.com>
2023-04-13 12:53:01 +0100

    Add StreamId.gen4, cleanup (#378)

--- src/Equinox/StreamId.fs
+++ src/Equinox/StreamId.fs
@@ -9,14 +9,6 @@ module StreamName =
     let inline validateCategoryName (rawCategory : string) =
         if rawCategory |> String.IsNullOrEmpty then invalidArg (nameof rawCategory) "may not be null or empty"
         if rawCategory.IndexOf '-' <> -1 then invalidArg (nameof rawCategory) "may not contain embedded '-' symbols"
-    /// Throws if a candidate streamId fragment includes a '_', is null, or is empty
-    let inline validateStreamIdFragment (rawFragment : string) =
-        if rawFragment |> String.IsNullOrEmpty then invalidArg (nameof rawFragment) "may not contain null or empty fragments"
-        if rawFragment.IndexOf '_' <> -1 then invalidArg (nameof rawFragment) "may not contain embedded '_' symbols"
-    /// Combines streamId fragments. Throws if any of the fragments embed a `_`, are `null`, or are empty
-    let combineStreamIdFragments (fragments : string seq) : string =
-        fragments |> Seq.iter validateStreamIdFragment
-        String.Join("_", fragments)
     /// Render in canonical {categoryName}-{streamId} format. Throws if categoryName contains embedded `-` symbols
     let render categoryName streamId =
         validateCategoryName categoryName
@@ -25,11 +17,24 @@ module StreamName =
 /// Represents the second half of a canonical StreamName, i.e., the streamId in "{categoryName}-{streamId}"
 type StreamId = string<streamId>
 and [<Measure>] streamId
-/// Helpers for composing and rendering StreamId values
+/// Low-level helpers for composing and rendering StreamId values; prefer the ones in the Equinox namespace
 module StreamId =
 
+    /// Throws if a candidate streamId fragment includes a '_', is null, or is empty
+    let inline validateStreamIdFragment (rawFragment : string) =
+        if rawFragment |> String.IsNullOrEmpty then invalidArg (nameof rawFragment) "may not contain null or empty fragments"
+        if rawFragment.IndexOf '_' <> -1 then invalidArg (nameof rawFragment) "may not contain embedded '_' symbols"
     /// Create a StreamId, trusting the input to be well-formed (see the gen* functions for composing with validation)
     let ofRaw (raw : string) : StreamId = UMX.tag raw
+    /// Validates and generates a StreamId from an application level fragment. Throws if any of the fragments embed a `_`, are `null`, or are empty
+    let ofFragment (fragment : string) : StreamId =
+        // arguably rejection of `_` chars is a step too far, but this accommodates for more easily dealing with namespacing dictated by unforeseen needs
+        validateStreamIdFragment fragment
+        ofRaw fragment
+    /// Combines streamId fragments. Throws if any of the fragments embed a `_`, are `null`, or are empty
+    let ofFragments (fragments : string seq) : StreamId =
+        fragments |> Seq.iter validateStreamIdFragment
+        String.Join("_", fragments) |> ofRaw
     /// Render as a string for external use
     let toString : StreamId -> string = UMX.untag
     /// Render as a canonical "{categoryName}-{streamId}" StreamName. Throws if the categoryName embeds `-` chars.
@@ -41,14 +46,13 @@ namespace Equinox
 type StreamId =
 
     /// Generate a StreamId from a single application-level id, given a rendering function that maps to a non empty fragment without embedded `_` chars
-    static member Map(f : 'a -> string) = System.Func<'a, Core.StreamId>(fun id ->
-        let element = f id
-        Core.StreamName.validateStreamIdFragment element
-        Core.StreamId.ofRaw element)
-    /// Generate a StreamId from a tuple of application-level ids, given two rendering functions that map to a non empty fragment without embedded `_` chars
-    static member Map(f, f2) = System.Func<_, _, _>(fun id1 id2 -> seq { yield f id1; yield f2 id2 } |> Core.StreamName.combineStreamIdFragments |> Core.StreamId.ofRaw)
-    /// Generate a StreamId from a triple of application-level ids, given three rendering functions that map to a non empty fragment without embedded `_` chars
-    static member Map(f1, f2, f3) = System.Func<_, _, _, _>(fun id1 id2 id3 -> seq { yield f1 id1; yield f2 id2; yield f3 id3 } |> Core.StreamName.combineStreamIdFragments |> Core.StreamId.ofRaw)
+    static member Map(f : 'a -> string) = System.Func<'a, Core.StreamId>(fun id -> Core.StreamId.ofFragment (f id))
+    /// Generate a StreamId from a tuple of application-level ids, given 2 rendering functions that map to a non empty fragment without embedded `_` chars
+    static member Map(f, f2) = System.Func<_, _, _>(fun id1 id2 -> Core.StreamId.ofFragments (seq { yield f id1; yield f2 id2 }))
+    /// Generate a StreamId from a triple of application-level ids, given 3 rendering functions that map to a non empty fragment without embedded `_` chars
+    static member Map(f1, f2, f3) = System.Func<_, _, _, _>(fun id1 id2 id3 -> Core.StreamId.ofFragments (seq { yield f1 id1; yield f2 id2; yield f3 id3 }))
+    /// Generate a StreamId from a 4-tuple of application-level ids, given 4 rendering functions that map to a non empty fragment without embedded `_` chars
+    static member Map(f1, f2, f3, f4) = System.Func<_, _, _, _, _>(fun id1 id2 id3 id4 -> Core.StreamId.ofFragments (seq { yield f1 id1; yield f2 id2; yield f3 id3; yield f4 id4 }))
 
 /// Helpers for composing and rendering StreamId values
 module StreamId =
@@ -59,3 +63,5 @@ module StreamId =
     let gen2 f1 f2 : 'a * 'b -> Core.StreamId = StreamId.Map(f1, f2).Invoke
     /// Generate a StreamId from a triple of application-level ids, given three rendering functions that map to a non empty fragment without embedded `_` chars
     let gen3 f1 f2 f3 : 'a * 'b * 'c -> Core.StreamId = StreamId.Map(f1, f2, f3).Invoke
+    /// Generate a StreamId from a 4-tuple of application-level ids, given four rendering functions that map to a non empty fragment without embedded `_` chars
+    let gen4 f1 f2 f3 f4 : 'a * 'b * 'c * 'd -> Core.StreamId = StreamId.Map(f1, f2, f3, f4).Invoke
