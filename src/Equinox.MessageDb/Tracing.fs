3f46230b8a23ce45e5c624175f81a6db5f0826a6
Einar Norðfjörð <einar@nordfjord.io>
2023-05-15 23:24:07 -0400

    dead code removala

--- src/Equinox.MessageDb/Tracing.fs
+++ src/Equinox.MessageDb/Tracing.fs
@@ -11,15 +11,6 @@ let source = new ActivitySource("Equinox.MessageDb")
 type ActivityExtensions =
 
     [<System.Runtime.CompilerServices.Extension>]
-    static member AddStreamFromParent(act: Activity, parent: Activity) =
-        if parent <> null then
-            let streamName = parent.GetTagItem("eqx.stream_name") |> unbox<string>
-            let streamId = parent.GetTagItem("eqx.stream_id") |> unbox<string>
-            let category = parent.GetTagItem("eqx.category") |> unbox<string>
-            act.AddStream(streamName, streamId, category) |> ignore
-        act
-
-    [<System.Runtime.CompilerServices.Extension>]
     static member AddExpectedVersion(act: Activity, version) =
         match version with StreamVersion v -> act.AddTag("eqx.expected_version", v) | Any -> act
 
@@ -32,10 +23,6 @@ type ActivityExtensions =
         act.AddTag("eqx.batch_size", size)
 
     [<System.Runtime.CompilerServices.Extension>]
-    static member AddBatch(act: Activity, size: int64, index: int) =
-        act.AddBatchSize(size).AddTag("eqx.batch_index", index)
-
-    [<System.Runtime.CompilerServices.Extension>]
     static member AddBatches(act: Activity, batches: int) =
         act.AddTag("eqx.batches", batches)
 
@@ -44,13 +31,5 @@ type ActivityExtensions =
         act.AddTag("eqx.start_position", pos)
 
     [<System.Runtime.CompilerServices.Extension>]
-    static member AddStale(act: Activity, allowStale: bool) =
-        act.AddTag("eqx.allow_stale", allowStale)
-
-    [<System.Runtime.CompilerServices.Extension>]
     static member AddLoadMethod(act: Activity, method: string) =
         act.AddTag("eqx.load_method", method)
-
-    [<System.Runtime.CompilerServices.Extension>]
-    static member RecordConflict(act: Activity) =
-        act.AddTag("eqx.conflict", true).SetStatus(ActivityStatusCode.Error, "WrongExpectedVersion")
