5cd8829a5082a76b678bf2d507bfa89a41e8fcb2
Ruben Bartelink <ruben@bartelink.com>
2023-06-06 00:23:19 +0100

    Update TaskSeq to 0.4.0-alpha.1 (#391)

--- src/Equinox.Core/Internal.fs
+++ src/Equinox.Core/Internal.fs
@@ -11,37 +11,3 @@ module Log =
     let [<return: Struct>] (|ScalarValue|_|): Serilog.Events.LogEventPropertyValue -> obj voption = function
         | :? Serilog.Events.ScalarValue as x -> ValueSome x.Value
         | _ -> ValueNone
-
-#if !NO_TASK_SEQ
-module TaskSeq =
-
-    open FSharp.Control
-
-    let takeWhile predicate (source: taskSeq<_>) = taskSeq {
-        use e = source.GetAsyncEnumerator(System.Threading.CancellationToken())
-        let! step = e.MoveNextAsync()
-        let mutable go = step
-        while go do
-            let value = e.Current
-            if predicate value then
-                yield value
-                let! more = e.MoveNextAsync()
-                go <- more
-            else
-                go <- false
-    }
-
-    let takeWhileInclusive predicate (source: taskSeq<_>) = taskSeq {
-        use e = source.GetAsyncEnumerator(System.Threading.CancellationToken())
-        let! step = e.MoveNextAsync()
-        let mutable go = step
-        while go do
-            let value = e.Current
-            yield value
-            if predicate value then
-                let! more = e.MoveNextAsync()
-                go <- more
-            else
-                go <- false
-    }
-#endif
