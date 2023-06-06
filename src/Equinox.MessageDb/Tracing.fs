289d74320ab368b930f1ee5db614c370ca24dfff
Einar Norðfjörð <einar@nordfjord.io>
2023-06-06 12:58:46 -0400

    Improve mdb tracing by creating fewer spans

--- src/Equinox.MessageDb/Tracing.fs
+++ src/Equinox.MessageDb/Tracing.fs
@@ -5,19 +5,8 @@ open Equinox.Core.Tracing
 open System.Diagnostics
 open Equinox.MessageDb.Core
 
-let source = new ActivitySource("Equinox.MessageDb")
-
 [<System.Runtime.CompilerServices.Extension>]
 type ActivityExtensions =
-
-    [<System.Runtime.CompilerServices.Extension>]
-    static member AddStreamFromParent(act: Activity, parent: Activity) =
-        if parent <> null then
-            let streamName = parent.GetTagItem("eqx.stream_name") |> unbox<string>
-            let streamId = parent.GetTagItem("eqx.stream_id") |> unbox<string>
-            let category = parent.GetTagItem("eqx.category") |> unbox<string>
-            act.AddStream(streamName, streamId, category) |> ignore
-        act
 
     [<System.Runtime.CompilerServices.Extension>]
     static member AddExpectedVersion(act: Activity, version) =
@@ -30,10 +19,6 @@ type ActivityExtensions =
     [<System.Runtime.CompilerServices.Extension>]
     static member AddBatchSize(act: Activity, size: int64) =
         act.AddTag("eqx.batch_size", size)
-
-    [<System.Runtime.CompilerServices.Extension>]
-    static member AddBatch(act: Activity, size: int64, index: int) =
-        act.AddBatchSize(size).AddTag("eqx.batch_index", index)
 
     [<System.Runtime.CompilerServices.Extension>]
     static member AddBatches(act: Activity, batches: int) =
