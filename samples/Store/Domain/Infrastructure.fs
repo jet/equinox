4bfa19ef9150a1d613572996b68f00631cecd4ed
Ruben Bartelink <ruben@bartelink.com>
2023-08-08 11:32:53 +0100

    Target packaged FsCodec

--- samples/Store/Domain/Infrastructure.fs
+++ samples/Store/Domain/Infrastructure.fs
@@ -8,7 +8,7 @@ open System
 /// Endows any type that inherits this class with standard .NET comparison semantics using a supplied token identifier
 [<AbstractClass>]
 type Comparable<'TComp, 'Token when 'TComp :> Comparable<'TComp, 'Token> and 'Token : comparison>(token: 'Token) =
-    member private _.Token = token
+    member val private Token = token
     override x.Equals y = match y with :? Comparable<'TComp, 'Token> as y -> x.Token = y.Token | _ -> false
     override _.GetHashCode() = hash token
     interface IComparable with
