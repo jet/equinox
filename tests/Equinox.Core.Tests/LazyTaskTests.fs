4aab4a2c8b880340e3150b8cbbc7e046926553a2
Ruben Bartelink <ruben@bartelink.com>
2024-04-08 22:58:46 +0100

    fix(Caching): Correct Cancellation Handling (#451)

--- tests/Equinox.Core.Tests/LazyTaskTests.fs
+++ tests/Equinox.Core.Tests/LazyTaskTests.fs
@@ -19,3 +19,42 @@ let [<Fact>] ``LazyTask.Empty is a true singleton, does not allocate`` () =
     let i1 = LazyTask<int>.Empty
     let i2 = LazyTask<int>.Empty
     test <@ obj.ReferenceEquals(i1, i2) @>
+
+[<Theory; InlineData false; InlineData true>]
+let ``LazyTask TryAwaitValid fault handling`` immediately = async {
+    let expected = if immediately then "bad beginning" else "bad ending"
+    let cell = LazyTask(fun () -> task {  if immediately then failwith "bad beginning"
+                                          do! Task.Delay 10
+                                          failwith "bad ending" })
+    let res = cell.TryAwaitValid()
+    // We've not awaited it yet, so nothing bad yet
+    test <@ not res.IsFaulted @>
+    let! res = res |> Async.AwaitTaskCorrect |> Async.Catch
+    // Depending on whether there's a continuation in the task, we'll see different outcomes
+    if immediately then Choice1Of2 ValueNone =! res
+    else test <@ match res with Choice2Of2 e -> e.Message = expected | _ -> false @>
+
+    let! res2 = cell.TryAwaitValid() |> Async.AwaitTaskCorrect
+    // next attempt does not propagate the fault
+    res2 =! ValueOption.None }
+
+[<Theory; InlineData false; InlineData true>]
+let ``LazyTask TryAwaitValid cancellation`` immediately = async {
+    let cts = new System.Threading.CancellationTokenSource()
+    let cell: LazyTask<unit> =
+        if immediately then
+            cts.Cancel()
+            LazyTask(fun () -> Task.FromCanceled<unit> cts.Token)
+        else
+            LazyTask(fun () -> task { do! Task.Delay(10000, cts.Token)
+                                      return () })
+    if immediately then
+        let! res = cell.TryAwaitValid() |> Async.AwaitTaskCorrect
+        res =! ValueNone
+    else
+        cts.CancelAfter 100
+        let! res = cell.TryAwaitValid() |> Async.AwaitTaskCorrect |> Async.Catch
+        test <@ match res with Choice2Of2 (:? TaskCanceledException) -> true | _ -> false @>
+    // Next time, we know it's faulted
+    let! res = cell.TryAwaitValid() |> Async.AwaitTaskCorrect
+    res =! ValueOption.None }
