dbfb27c76a1dd954d57d823d52a5b9a10b68a27b
Ruben Bartelink <ruben@bartelink.com>
2025-01-01 22:06:22 +0000

    Cover TryLoad/TryHydrateTip logic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -122,7 +122,7 @@ The `Unreleased` section name is replaced by the expected version of next releas
 - `CosmosStore`: Only log `bytes` when log level is `Debug` [#305](https://github.com/jet/equinox/pull/305)
 - `CosmosStore.AccessStrategy.MultiSnapshot`,`Custom`: Change `list` and `seq` types to `array` [#338](https://github.com/jet/equinox/pull/338)
 - `CosmosStore.CosmosStoreCategory`: Generalize `compressUnfolds` to `shouldCompress` predicate [#436](https://github.com/jet/equinox/pull/436)
-- `CosmosStore.CosmosStoreCategory.TryHydrateTip`: Generates a Stream State Momento based on externally loaded `u`nfold state [#434](https://github.com/jet/equinox/pull/434)
+- `CosmosStore.CosmosStoreCategory.TryHydrateTip`: Generates a Stream State Memento based on externally loaded `u`nfold state [#434](https://github.com/jet/equinox/pull/434)
 - `CosmosStore.CosmosStoreCategory.TryLoad`: Renders a `'state` based on an Unfold [#434](https://github.com/jet/equinox/pull/434)
 - `CosmosStore.Core.Initialization.initAux`: Replace hard-coded manual 400 RU with `mode` parameter [#328](https://github.com/jet/equinox/pull/328) :pray: [@brihadish](https://github.com/brihadish)
 - `CosmosStore.CosmosClientFactory`: Moved to Core [#430](https://github.com/jet/equinox/pull/430)
--- tests/Equinox.CosmosStore.Integration/FsCodecCompressionTests.fs
+++ tests/Equinox.CosmosStore.Integration/FsCodecCompressionTests.fs
@@ -1,15 +1,15 @@
 ﻿// Prior to version v 4.1.0, CosmosStore owned:
 // - compression of snapshots (and APIs controlling conditionally of that)
 // - inflation of snapshots
-// This is now an external concern, fully implemented by APIs presented in FsCodec.SystemTextJson.Compression V 3.1.0 and later
+// This is now an external concern, fully implemented by APIs presented in FsCodec.SystemTextJson.Compression v 3.1.0 and later
 // These tests are a sanity check pinning the basic mechanisms that are now externalized; any more thorough tests should be maintained in FsCodec
 // NOTE there is no strong dependency on FsCodec; CosmosStore is happy to roundtrip arbitrary pairs of D/d and M/m values
-//      However, it is recommended to use that implementation as it provides for interop with (Deflate-compressed) snapshots as written by CosmosStore pre 4.1.0
 // NOTE prior to v 4.1.0, CosmosStore provided a System.Text.Json integration for Microsoft.Azure.Cosmos
 //      Version 4.1.0 and later lean on the integrated support provided from Microsoft.Azure.Cosmos v 3.43.0 onward
 module Equinox.CosmosStore.Integration.FsCodecCompressionTests
 
 open Equinox.CosmosStore
+open Equinox.Store.Integration
 open FsCheck.Xunit
 open Swensen.Unquote
 open System
@@ -24,7 +24,6 @@ type Union =
 type CoreBehaviors() =
     let defaultOptions = System.Text.Json.JsonSerializerOptions.Default // Rule out dependencies on any FsCodec conversions
     let eventCodec = FsCodec.SystemTextJson.CodecJsonElement.Create(defaultOptions)
-    let nullElement = System.Text.Json.JsonSerializer.SerializeToElement null
 
     let ser_ et struct (D, d) =
         let e: Core.Unfold =
@@ -32,7 +31,7 @@ type CoreBehaviors() =
                 c = et
                 d = d
                 D = D
-                m = nullElement
+                m = Unchecked.defaultof<_>
                 M = Unchecked.defaultof<int>
                 t = DateTimeOffset.MinValue }
         System.Text.Json.JsonSerializer.Serialize e
@@ -40,14 +39,14 @@ type CoreBehaviors() =
 
     [<Fact>]
     let ``serializes, achieving expected compression`` () =
-        let encoded = eventCodec |> FsCodec.SystemTextJson.Compression.EncodeTryCompress |> _.Encode((), A { embed = String('x',5000) })
+        let encoded = eventCodec |> FsCodec.SystemTextJson.EncodedBody.EncodeTryCompress |> _.Encode((), A { embed = String('x',5000) })
         let res = ser encoded
         test <@ res.Contains "\"d\":\"" && res.Length < 138 && res.Contains "\"D\":2" @>
 
     let codec compress =
         let forceCompression: FsCodec.SystemTextJson.CompressionOptions = { minSize = 0; minGain = -1000 }
-        if compress then FsCodec.SystemTextJson.Compression.EncodeTryCompress(eventCodec, forceCompression)
-        else FsCodec.SystemTextJson.Compression.EncodeUncompressed eventCodec
+        if compress then FsCodec.SystemTextJson.EncodedBody.EncodeTryCompress(eventCodec, forceCompression)
+        else FsCodec.SystemTextJson.EncodedBody.EncodeUncompressed eventCodec
 
     [<Property>]
     let roundtrips compress value =
@@ -63,6 +62,32 @@ type CoreBehaviors() =
 
     [<Fact>]
     let handlesNulls () =
-        let ser = ser_ "et" (0, nullElement)
+        let ser = ser_ "et" (0, System.Text.Json.JsonSerializer.SerializeToElement null)
         let des = System.Text.Json.JsonSerializer.Deserialize<Core.Unfold>(ser)
         test <@ System.Text.Json.JsonValueKind.Null = let d = des.d in d.ValueKind @>
+
+module Pre41IntegratedDeflate =
+    let private deflate (uncompressedBytes: byte[]) =
+        let output = new System.IO.MemoryStream()
+        let compressor = new System.IO.Compression.DeflateStream(output, System.IO.Compression.CompressionLevel.Optimal, leaveOpen = true)
+        compressor.Write(uncompressedBytes)
+        compressor.Flush() // Could `Close`, but not required
+        output.ToArray()
+    let encode (x: 't) = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes x |> deflate |> Convert.ToBase64String |> System.Text.Json.JsonSerializer.SerializeToElement
+
+type BackCompatTests() =
+
+    /// Validates that an Unfold with a compressed `d` value without an associated `D` value of `1` is correctly inflated via Core.EncodedBody.parseUnfold
+    [<AutoData(SkipIfRequestedViaEnvironmentVariable="EQUINOX_INTEGRATION_SKIP_COSMOS")>]
+    let ```Can inflate pre v 4.1 Deflate encoded snapshots`` () = Async.RunSynchronously <| async {
+        let context = createPrimaryContext Serilog.Log.Logger 10
+        let cat = DocumentStoreIntegration.Cart.createCategorySnapshot context
+        let expected: Domain.Cart.Fold.State = {
+            items = [
+                { skuId = SkuId Guid.Empty; quantity = 4; returnsWaived = Some true } ] }
+        TypeShape.Empty.register (fun () -> Unchecked.defaultof<System.Text.Json.JsonElement>)
+        let unfold = {
+            TypeShape.Empty.empty<Equinox.CosmosStore.Core.Unfold> with
+                c = nameof Domain.Cart.Events.Event.Snapshotted
+                d = Pre41IntegratedDeflate.encode expected }
+        Some expected =! cat.TryLoad [| unfold |] }
