d163bc7a282931d276eb08fc4784c569174b801d
Ruben Bartelink <ruben@bartelink.com>
2026-04-17 17:28:11 +0100

    Consolidate tracing.fs

--- src/Equinox.EventStoreDb/Tracing.fs
+++ src/Equinox.EventStoreDb/Tracing.fs
@@ -6,11 +6,17 @@ module internal
 #if STORE_EVENTSTORE_LEGACY
     Equinox.EventStore.Tracing
 #endif
-#if !STORE_EVENTSTOREDB && !STORE_EVENTSTORE_LEGACY
+#if STORE_MESSAGEDB
+    Equinox.MessageDb.Tracing
+#endif
+#if !STORE_EVENTSTOREDB && !STORE_EVENTSTORE_LEGACY && !STORE_MESSAGEDB
     Equinox.SqlStreamStore.Tracing
 #endif
 
 open System.Diagnostics
+#if STORE_MESSAGEDB
+open Equinox.MessageDb.Core
+#endif
 
 let [<Literal>] private SourceName =
 #if STORE_EVENTSTOREDB
@@ -19,7 +25,10 @@ let [<Literal>] private SourceName =
 #if STORE_EVENTSTORE_LEGACY
     "Equinox.EventStore"
 #endif
-#if !STORE_EVENTSTOREDB && !STORE_EVENTSTORE_LEGACY
+#if STORE_MESSAGEDB
+    "Equinox.MessageDb"
+#endif
+#if !STORE_EVENTSTOREDB && !STORE_EVENTSTORE_LEGACY && !STORE_MESSAGEDB
     "Equinox.SqlStreamStore"
 #endif
 
@@ -28,21 +37,28 @@ let source = new ActivitySource(SourceName)
 [<AbstractClass; Sealed; System.Runtime.CompilerServices.Extension>]
 type ActivityExtensions private () =
 
+#if STORE_MESSAGEDB
+    [<System.Runtime.CompilerServices.Extension>]
+    static member AddExpectedVersion(act: Activity, version) =
+        match version with StreamVersion v -> act.AddTag("eqx.expected_version", v) | Any -> act
+#else
     [<System.Runtime.CompilerServices.Extension>]
     static member AddExpectedVersion(act: Activity, version: int64) =
         act.AddTag("eqx.expected_version", version)
+#endif
 
     [<System.Runtime.CompilerServices.Extension>]
     static member AddLastVersion(act: Activity, version: int64) =
         act.AddTag("eqx.last_version", version)
 
     [<System.Runtime.CompilerServices.Extension>]
-    static member AddBatchSize(act: Activity, size: int) =
-        act.AddTag("eqx.batch_size", size)
-
-    [<System.Runtime.CompilerServices.Extension>]
     static member AddBatches(act: Activity, batches: int) =
         act.AddTag("eqx.batches", batches)
+
+#if STORE_MESSAGEDB
+    [<System.Runtime.CompilerServices.Extension>]
+    static member AddBatchSize(act: Activity, size: int64) =
+        act.AddTag("eqx.batch_size", size)
 
     [<System.Runtime.CompilerServices.Extension>]
     static member AddStartPosition(act: Activity, pos: int64) =
@@ -51,10 +67,11 @@ type ActivityExtensions private () =
     [<System.Runtime.CompilerServices.Extension>]
     static member AddLoadMethod(act: Activity, method: string) =
         act.AddTag("eqx.load_method", method)
-
+#else
     [<System.Runtime.CompilerServices.Extension>]
     static member AddDirection(act: Activity, direction: string) =
         act.AddTag("eqx.direction", direction)
+#endif
 
     [<System.Runtime.CompilerServices.Extension>]
     static member RecordConflict(act: Activity) =
